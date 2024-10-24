d14275fae4f28c016bba4264f57de16b105997fa
Tenko <tenko_92@mail.ru>
2024-10-24 19:45:35 +0500

    update create_database.sql file

--- src/main/resources/db/changelog/changeset/create_database.sql
+++ src/main/resources/db/changelog/changeset/create_database.sql
@@ -8,12 +8,12 @@ UNIQUE (num)
 
 CREATE TABLE IF NOT EXISTS client_limit (
 id INT NOT NULL AUTO_INCREMENT,
-client_account_id INT,
+client_account_id INT NOT NULL,
 date_time TIMESTAMP,
-amount DECIMAL(9,2) DEFAULT '1000.00',
+amount DECIMAL(15,2) DEFAULT '1000.00',
 category VARCHAR (10),
-rest DECIMAL(9,2),
-currency VARCHAR(3) DEFAULT 'USD',
+rest DECIMAL(15,2),
+currency VARCHAR(3) NOT NULL DEFAULT 'USD',
 PRIMARY KEY(id),
 FOREIGN KEY (client_account_id) REFERENCES client_account(id)
 );
@@ -22,7 +22,7 @@ CREATE TABLE IF NOT EXISTS client_transaction (
 id INT NOT NULL AUTO_INCREMENT,
 client_account_id INT NOT NULL,
 target_acc_num INT(10),
-purchase_amount DECIMAL(9,2),
+purchase_amount DECIMAL(15,2),
 category VARCHAR(10),
 date_time TIMESTAMP,
 currency VARCHAR(5),
@@ -34,7 +34,7 @@ FOREIGN KEY (client_account_id) REFERENCES client_account(id)
 CREATE TABLE IF NOT EXISTS currency_rate (
 id INT NOT NULL AUTO_INCREMENT,
 date_rate DATE,
-rate_USD_KZ DECIMAL(9,2),
-rate_USD_RU DECIMAL(9,2),
+rate_USD_KZ DECIMAL(15,2),
+rate_USD_RU DECIMAL(15,2),
 PRIMARY KEY(id)
 );
